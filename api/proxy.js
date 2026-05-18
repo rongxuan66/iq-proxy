69e38456959da9cafdc92fb7aea849841b14f5c5
荣轩 <93523049+rongxuan66@users.noreply.github.com>
2026-05-18 22:22:26 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,11 @@
 export default async function handler(req, res) {
   const params = new URLSearchParams(req.query).toString()
-  const resp = await fetch(`https://admin.iqapi.cn/ajax.php?${params}`)
+  const resp = await fetch(`https://admin.iqapi.cn/ajax.php?${params}`, {
+    headers: {
+      "Referer": "https://admin.iqapi.cn",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0 Safari/537.36"
+    }
+  })
   const data = await resp.text()
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.status(resp.status).send(data)
